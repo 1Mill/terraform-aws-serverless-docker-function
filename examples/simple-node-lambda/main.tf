ff8e59dfd11eec8618af1b55fa2772b89dff95e8
1Mill <erik.ekberg@outlook.com>
2022-04-28 15:50:01 -0700

    Improve working example

--- examples/simple-node-lambda/main.tf
+++ examples/simple-node-lambda/main.tf
@@ -1,3 +1,19 @@
+terraform {
+	required_providers {
+		aws = {
+			source = "hashicorp/aws"
+			version = "~> 4.11"
+		}
+	}
+}
+
+data "aws_iam_policy_document" "this" {
+	statement {
+		actions = ["kms:Decrypt"]
+		resources = ["*"]
+		sid = "MyUniqueDescription"
+	}
+}
 module "serverless-docker-function" {
 	source  = "../.."
 
@@ -6,5 +22,9 @@ module "serverless-docker-function" {
 		MESSAGE = "Hello from the environment!"
 		NODE_ENV = "production"
 	}
-	function = { name = "my-simple-node-lambda" }
+	function = {
+		name = "my-simple-node-lambda"
+		version = "v0.0.1"
+	}
+	policy = data.aws_iam_policy_document.this.json
 }
