6bf67ebc3a956fdd3152a68485c1473a94d70b0c
1Mill <erik.ekberg@outlook.com>
2022-08-06 20:02:48 -0700

    Be explicit rather than implicit for version

--- providers.tf
+++ providers.tf
@@ -23,5 +23,5 @@ terraform {
 	# ! Terraform v1.3 introduces a breaking change by throwing
 	# ! an error when using experiment "module_variable_optional_attrs"
 	# ! and by removing the "defaults(..., ...)" helper function
-	required_version = "~> 1, < 1.3"
+	required_version = "~> 1.0, < 1.3"
 }
